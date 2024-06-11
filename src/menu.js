dd3ba3db1b09a41446c3d154c48964ab88491ac0
samuelauto <samuelme@automatica.edu.cujae.cu>
2024-06-11 17:51:49 -0400

    Resuelto problema de ilustrar la comida

--- src/menu.js
+++ src/menu.js
@@ -12,22 +12,21 @@ function Menu(){
     for(let i = 0; i<4; i++){
         const section = document.createElement('div');//div donde va cada uno
         const titleSection = document.createElement('h3');
-        const dish = document.createElement('p');
         div.appendChild(section);
         switch(i){
             case 0: titleSection.textContent = "Entrante"
                     section.appendChild(titleSection);
                     for(let j=0;j<3;j++){
-                        section.appendChild(dish);
+                        const dish = document.createElement('p');
                         switch(j){
                             case 0: dish.textContent = "Croquetas de la casa:.................600 cup";
-                                    //section.appendChild(dish);
+                                    section.appendChild(dish);
                                     break;
                             case 1: dish.textContent = "Maripositas chinas:...............500 cup";
-                                    //section.appendChild(dish);
+                                    section.appendChild(dish);
                                     break;
                             case 2: dish.textContent = "Picadera mixta:.....................1500cup";
-                                    //section.appendChild(dish);
+                                    section.appendChild(dish);
                                     break;
                             default: break;
                         }
@@ -36,6 +35,7 @@ function Menu(){
             case 1: titleSection.textContent = "Platos Principales";
                     section.appendChild(titleSection);
                     for(let j=0;j<3;j++){
+                        const dish = document.createElement('p');
                         switch(j){
                             case 0: dish.textContent = "Arroz con pollo:.......................1200 cup";
                                     section.appendChild(dish);
@@ -53,6 +53,7 @@ function Menu(){
             case 2: titleSection.textContent = "Postres";
                     section.appendChild(titleSection);
                     for(let j=0;j<3;j++){
+                        const dish = document.createElement('p');
                         switch(j){
                             case 0: dish.textContent = "Flancito de la pura:.......................500 cup";
                                     section.appendChild(dish);
@@ -70,6 +71,7 @@ function Menu(){
             case 3: titleSection.textContent = "Bebidas";
                     section.appendChild(titleSection);
                     for(let j=0;j<3;j++){
+                        const dish = document.createElement('p');
                         switch(j){
                             case 0: dish.textContent = "Agua fresca de jamaica:....................300 cup";
                                     section.appendChild(dish);
